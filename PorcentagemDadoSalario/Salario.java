c1ceb8ad6ae08db1cecfa39f132a75f1c997754c
PaiNxc <samuelmello1234@hotmail.com>
2023-02-03 19:13:41 -0300

    Calculadora de Porcentagem de um Salario
    Fiz uma simples calculadora que tira uma determinada porcentagem de um salario.

---
+++ PorcentagemDadoSalario/Salario.java
@@ -0,0 +1,10 @@
+package PorcentagemDadoSalario;
+
+public class Salario {
+    public static void main(String[] args){
+        float wage = 1276.00F;
+        float percent = 76;
+        float result = wage / 100 * percent;
+        System.out.println(result);
+    }
+}
