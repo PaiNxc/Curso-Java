d4562416100c02114e9a0ea1d318fe2da20e9ab5
PaiNxc <samuelmello1234@hotmail.com>
2023-02-03 18:34:50 -0300

    Calculadora de Diferença de Idade
    Criei um simples código que calcula a diferença entre a idade de duas pessoas, primeira vez que utilizo uma variável.

---
+++ CalculadorDiferencaIdade/CalculadorDiferencaIdade.java
@@ -0,0 +1,10 @@
+package CalculadorDiferencaIdade;
+
+public class CalculadorDiferencaIdade {
+    public static void main(String[] args){
+        int firstAge = 30;
+        int secondAge = 19;
+        int result = firstAge - secondAge;
+        System.out.println(result);
+    }
+}
