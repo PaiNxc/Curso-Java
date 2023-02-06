6fc1b2d81e26ab40b17a1d0cf0ed9e049c895b72
PaiNxc <samuelmello1234@hotmail.com>
2023-02-06 10:17:08 -0300

    Estrutura IF
    Aprendendo a utilizar a estrutura condicional If e Else

---
+++ CondicionaisIF/Condicional.java
@@ -0,0 +1,12 @@
+package CondicionaisIF;
+
+public class Condicional{
+public static void main(String[] args) {
+        int idade = 24;
+        if(idade > 18){
+            System.out.println("Você é maior de idade");
+        } else {
+        System.out.println("Você é menor de idade");
+        }
+    }
+}
